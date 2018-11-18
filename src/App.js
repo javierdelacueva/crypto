096c360b04e7d683b09d62d268c5bd38759d2d4d
Javier de la Cueva Orts <javier.de.la.cueva.orts@everis.com>
2018-11-18 02:15:59 +0100

    Adding UI Framework and CSS properties for panels and loading

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Currency from 'react-currency-formatter';
+import { Panel, Row, Col } from "react-bootstrap";
+import { ClipLoader } from 'react-spinners';
 
 import './App.css';
 
@@ -13,7 +15,8 @@ class App extends Component {
     bitcoin: 0,
     litecoin: 0,
     ethereum: 0,
-    countdown: 30
+    countdown: 30,
+    loading: false,
   }
   componentDidMount() {
     this.getCurrencies();
@@ -35,7 +38,7 @@ class App extends Component {
     clearInterval(this.interval);
   }
   getCurrencies() {
-    console.log('start');
+    this.setState({ loading: true })
     axios.all([
       http.get(`ticker/1/`),
       http.get(`ticker/2/`),
@@ -45,7 +48,8 @@ class App extends Component {
       this.setState({ 
         bitcoin: bitcoin.data.data.quotes.USD.price,
         litecoin: litecoin.data.data.quotes.USD.price,
-        ethereum: ethereum.data.data.quotes.USD.price, 
+        ethereum: ethereum.data.data.quotes.USD.price,
+        loading: false,
       })
     }))
     .catch(error => console.log(error));
@@ -56,22 +60,49 @@ class App extends Component {
         <div className="App-header">
           <h1>CryptoWallet</h1>
           <div className="App-card">
-            | BTC:
-            <Currency
-              quantity={this.state.bitcoin}
-              currency="USD"
-            />
-             | ETH: 
-             <Currency
-              quantity={this.state.ethereum}
-              currency="USD"
-            />
-             | LTC: 
-             <Currency
-              quantity={this.state.litecoin}
-              currency="USD"
-            />
-             |
+            <div className="ClipLoader">
+              <ClipLoader
+                sizeUnit={"px"}
+                size={50}
+                color={'#CCCCC'}
+                loading={this.state.loading}
+              />
+          </div> 
+            <Row>
+              <Col xs={6} md={4}>
+                <Panel>
+                  <Panel.Body>
+                    <Currency
+                      quantity={this.state.bitcoin}
+                      currency="USD"
+                    />
+                  </Panel.Body>
+                  <Panel.Footer><p><b>BTC</b></p></Panel.Footer>
+                </Panel>
+              </Col>
+              <Col xs={6} md={4}>
+                <Panel>
+                  <Panel.Body>
+                    <Currency
+                      quantity={this.state.ethereum}
+                      currency="USD"
+                    />
+                  </Panel.Body>
+                  <Panel.Footer><p><b>ETH</b></p></Panel.Footer>
+                </Panel>
+              </Col>
+              <Col xsHidden md={4}>
+                <Panel>
+                  <Panel.Body>
+                    <Currency
+                      quantity={this.state.litecoin}
+                      currency="USD"
+                    />
+                  </Panel.Body>
+                  <Panel.Footer><p><b>LTC</b></p></Panel.Footer>
+                </Panel>
+              </Col>
+            </Row>
             <p className="App-subtext">
               Prices refresh in { this.state.countdown } seconds.
             </p>
